9b9705196c615319a69937999c7b499ef817fd0a
luciamra <luciaromeroalston@gmail.com>
2024-03-31 13:34:58 -0500

    attempt 2

--- ParkingGarage/src/core.jl
+++ ParkingGarage/src/core.jl
@@ -18,11 +18,11 @@ mutable struct ParkingGarageState{T<:AbstractFloat}
 end
 
 function ParkingGarageState()
-    #return ParkingGarageState(0, 1, calculate_demand(1, 80.0))
-    dgrs = [5.0,10.0,15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0,75.0,80.0,85.0,90.0,95.0] 
-    for i in 1:length(dgrs)
-        return ParkingGarageState(0, 1, calculate_demand(1, getindex(dgrs,i)))
-    end
+    return ParkingGarageState(0, 1, calculate_demand(1, 80.0))
+    #dgrs = [5.0,10.0,15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0,75.0,80.0,85.0,90.0,95.0] 
+    #for i in 1:length(dgrs)
+        #return ParkingGarageState(0, 1, calculate_demand(1, getindex(dgrs,i)))
+    #end
 end
 
 struct ParkingGarageAction
